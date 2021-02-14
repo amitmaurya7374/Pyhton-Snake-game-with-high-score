fe939c0b7590c9fdb94c53295d9df413bc84dd7f
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-14 11:35:43 +0530

    fix some bugs

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
@@ -23,11 +24,12 @@ class Scoreboard(Turtle):
 
     def reset_high_score(self):
         if self.score > self.highscore:
-            with open("data.txt",mode="w") as file:
-                updated_score = file.write(str(self.score))
-            self.highscore = int(updated_score)
+            self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.highscore))
         self.score = 0
         self.update_scoreboard()
+
     # def game_over(self):
     #     self.goto(0, 0)
     #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
